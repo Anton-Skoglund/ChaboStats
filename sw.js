f8fc00333effd43b52de0cd1238f3325c84f3ec9
Anton-Skoglund <anton.r.skoglund@gmail.com>
2025-09-01 09:18:46 +0200

    Updates

--- sw.js
+++ sw.js
@@ -1,9 +1,12 @@
+
 let listings = [];
 
 async function loadData() {
   if (!listings.length) {
-    const res = await fetch("/data.json");
+    const res = await fetch("/ChaboStats/data.json");
     listings = await res.json();
+    swLog("garb data")
+
   }
 }
 
@@ -11,13 +14,13 @@ self.addEventListener("fetch", (event) => {
   const url = new URL(event.request.url);
   const parts = url.pathname.split("/").filter(Boolean); // ["api", "listings", ...]
 
-  if (url.pathname.startsWith("/api/listings")) {
+  if (url.pathname.startsWith("/ChaboStats/api/listings")) {
     event.respondWith(
       (async () => {
         await loadData();
-
+        
         // /api/listings → return filtered by minSize/maxSize
-        if (parts.length === 2) {
+        if (parts.length === 3) {
           const qp = url.searchParams;
           const minSize = Number(qp.get("minSize")) || 0;
           const maxSize = Number(qp.get("maxSize")) || Infinity;
@@ -33,15 +36,14 @@ self.addEventListener("fetch", (event) => {
               listing.locationCode.toLowerCase().includes(locationCode)
           );
 
-
           return new Response(JSON.stringify(result), {
             headers: { "Content-Type": "application/json" },
           });
         }
 
         // /api/listings/:id → find by objectId
-        if (parts.length === 3) {
-          const id = parts[2];
+        if (parts.length === 4) {
+          const id = parts[3];
           const listing = listings.find((item) => item.objectId === id);
 
           if (listing) {
@@ -57,7 +59,7 @@ self.addEventListener("fetch", (event) => {
         }
 
         // /api/listings/:published/:objectId → find by published + objectId
-        if (parts.length === 4) {
+        if (parts.length === 5) {
           const [, , published, objectId] = parts;
           const listing = listings.find(
             (item) => item.published === published && item.objectId === objectId
@@ -83,14 +85,14 @@ self.addEventListener("fetch", (event) => {
     );
   }
 
-  if (url.pathname.startsWith("/api/location")) {
+  if (url.pathname.startsWith("/ChaboStats/api/location")) {
   event.respondWith(
     (async () => {
       await loadData();
 
       // /api/location/:code
-      if (parts.length === 3) {
-        const code = parts[2].toLowerCase();
+      if (parts.length === 4) {
+        const code = parts[3].toLowerCase();
         const result = listings.filter(
           (listing) => listing.locationCode.toLowerCase() === code
         );
